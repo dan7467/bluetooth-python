cff1d19ab47cd74dbb94183f95b55d586eb68591
dan7467 <dan7467@gmail.com>
2024-12-18 21:49:12 +0200

    fetch addresses and lookup names

--- app.py
+++ app.py
@@ -3,12 +3,9 @@ import bluetooth as bt
 def log(val, name):
     print(f'\n {name} = {val}\n')
 
-target_name = "S23 Dan"
-target_address = None
-
 nearby_devices = bt.discover_devices()
 
 log(nearby_devices, 'nearby_devices')
 
 for addr in nearby_devices:
-    log(bt.lookup_name(str(addr), 30), 'bt.lookup_name(str(addr), 30)')
+    log(bt.lookup_name(addr), 'bt.lookup_name(addr)')
