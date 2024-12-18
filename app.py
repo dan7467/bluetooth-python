0dfe49b6d9289274675e107ba6a92de4b9eddf3f
dan7467 <dan7467@gmail.com>
2024-12-18 21:26:50 +0200

    new virtual environment, with py3.11 instead of py3.12

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("hello world")
+import bluetooth as bt
+
+def log(val, name):
+    print(f'\n {name} = {val}\n')
+
+target_name = "S23 Dan"
+target_address = None
+
+nearby_devices = bt.discover_devices()
+
+log(nearby_devices, 'nearby_devices')
+
+for addr in nearby_devices:
+    log(bt.lookup_name(str(addr), 30), 'bt.lookup_name(str(addr), 30)')
